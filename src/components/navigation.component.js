b344065e60525ce930fdeb171b981df53755943d
Safarov <elkhan.safarov10@yandex.com>
2019-12-25 15:03:34 +0500

    Работающие табы

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,9 +3,15 @@ import { Component } from '../core/component';
 export class NavigationComponent extends Component {
     constructor(id) {
         super(id)
+
+        this.tabs = []
     }
     init() {
         this.$el.addEventListener('click', tabClickHandler.bind(this))
+    }
+
+    registerTabs(tabs) {
+        this.tabs = tabs
     }
 }
 function tabClickHandler(event) {
@@ -15,5 +21,9 @@ function tabClickHandler(event) {
             tab.classList.remove('active');
         });
         event.target.classList.add('active');
+
+        const activeTab = this.tabs.find(t => t.name === event.target.dataset.name);
+        this.tabs.forEach(t => t.component.hide());
+        activeTab.component.show();
     }
 }
